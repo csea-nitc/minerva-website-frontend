dd6726c7d18045d6828e4c554c2ec1f4ce07f8a8
Nandini <nandinimprakash.06@gmail.com>
2026-05-13 20:36:09 +0530

    Quick Links fixed

--- src/app/node/[slug]/page.js
+++ src/app/node/[slug]/page.js
@@ -21,7 +21,7 @@ export default function Home( { params : promiseParams } ) {
         const fetchData = async () => {
             try {
                 const data = await fetch(
-                    `${backend_url}/api/quick-links/${Slug}?populate[pdf][populate]=*&populate=image`,
+                    `${backend_url}/api/quick-links/${Slug}?populate=*`,
                     {
                         headers: {
                             Authorization: `Bearer ${token}`,
